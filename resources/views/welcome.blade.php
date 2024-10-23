0244d8f936f2a65c487cfeba90d21de2c02a92ab
Sittsak Phimthongphum <phimsittisak@gmail.com>
2024-10-23 22:26:51 +0700

    remove section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,6 @@
 								<li class="menu-item"><a class="menu-link" href="#"><div>Blog</div></a>
 								<li class="menu-item"><a class="menu-link" href="#"><div>Events</div></a>
 								<li class="menu-item"><a class="menu-link" href="#"><div>About VisionM</div></a>
-								<li class="menu-item"><a class="menu-link" href="#"><div>Careers</div></a>
 								<!-- <li class="menu-item"><a class="menu-link" href="#"><div>Contact us</div></a>
 									<ul class="sub-menu-container" data-class="up-lg:not-dark">
 										<li class="menu-item"><a class="menu-link" href="#"><div>NextGen Framework</div></a></li>
@@ -369,30 +368,6 @@
 							</div>
 						</div>
 
-					</div>
-				</div>
-
-				<!-- Team Work
-				============================================= -->
-				<div class="section bg-transparent pb-0 mb-0">
-					<div class="container">
-						<div class="row">
-
-							<div class="col-lg-5 order-lg-12">
-								<div class="device-xl device-md mt-6"></div>
-								<h3 class="mb-0" style="font-size: 40px;">Great Team Work.</h3>
-								<h3 class="fw-light ls-4 text-lowercase" style="font-size: 15px; color: #999;">Designed by Semicolonweb</h3>
-								<p>Uniquely plagiarize dynamic convergence after equity invested experiences. Holisticly repurpose installed base infomediaries before web-enabled methods of empowerment.</p>
-								<a href="#" class="button-link mb-5">Read More</a>
-							</div>
-
-							<div class="col-lg-7 order-lg-1">
-								<div style="position: relative; margin-bottom: 0;" class="overflow-hidden" data-height-xl="520" data-height-lg="520" data-height-md="520" data-height-sm="500" data-height-xs="300">
-									<img class="man" src="{{ asset('images/sections/man.jpg') }}" style="position: absolute; top: 0; left: auto;" alt="Chrome">
-								</div>
-							</div>
-
-						</div>
 					</div>
 				</div>
 
